61244194d3026fd4fe7dde9ff2e1beab0735d6da
Max Gordon <gordonmaxc@gmail.com>
2014-12-13 16:59:03 -0800

    setup test suite

---
+++ spec/models/organization_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require_relative '../../app/models/organization'
+
+describe Organization do
+  it "validates the presence of the name" do
+  end
+
+  xit "validates the uniqueness of the name" do
+  end
+end
